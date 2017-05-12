8e56eb186ce883f85fcd67181a399e9f5a7996a9
daniel.shih <daniel.shih@allpay.com.tw>
2017-05-12 10:21:13 +0800

    add FileModel.cs, add FileLimit Test

--- MVC_Simple/UploadTest/UnitTest1.cs
+++ MVC_Simple/UploadTest/UnitTest1.cs
@@ -11,23 +11,38 @@ namespace UploadTest
     {
         private UploadManage FileCollection() {
             List<HttpPostedFileBase> files = new List<HttpPostedFileBase>();
-            files.Add(new FakeFile("my.jpg"));
+            files.Add(new FakeFile(new FileModel() {
+                FileLenght = 1024 * 1024 * 6,
+                FileName = "my.jpg"
+            }));
             //files.Add(new FakeFile("my.doxds"));
             return new UploadManage(files);
         }
         [TestMethod]
-        public void FileFilter_Test()
+        public void FileFilter_True_Test()
         {
             var m= FileCollection();
             m.SetNext(new FileFilter(FileType.Image));
             bool IsOk=m.Execute();
             Assert.IsTrue(IsOk);
         }
+
+        [TestMethod]
+        public void Filelimit_False_Test()
+        {
+            var m = FileCollection();
+            m.SetNext(new Filelimit());
+            bool IsOk = m.Execute();
+            Assert.IsFalse(IsOk);
+        }
     } 
+
     public class FakeFile : HttpPostedFileBase {
         private string _filename;
-        public FakeFile(string filename) {
-            this._filename = filename;
+        private int _filelenght;
+        public FakeFile(FileModel model) {
+            _filename = model.FileName;
+            _filelenght = model.FileLenght;
         }
         public override string FileName
         {
@@ -36,5 +51,20 @@ namespace UploadTest
                 return _filename;
             }
         }
+        public override int ContentLength
+        {
+            get
+            {
+                return _filelenght;
+            }
+        }
     }
+
+    public class FileModel
+    {
+        public int FileLenght { get; set; }
+
+        public string FileName { get; set; }
+    }
+
 }
